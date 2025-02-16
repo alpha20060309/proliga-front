3fef9a5940babbd20e1777207ad9f7cb56fb80d9
Ramz001 <rkenjaev1@gmail.com>
2025-02-16 11:27:04 +0500

    supabase redirect config fix

--- src/app/(pages)/auth/callback/route.js
+++ src/app/(pages)/auth/callback/route.js
@@ -1,22 +1,40 @@
+// import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs"
+// import { cookies } from "next/headers"
+// import { NextResponse } from "next/server"
+
+// export async function GET(request) {
+//   const { searchParams } = new URL(request.url)
+//   const code = searchParams.get("code")
+
+//   if (code) {
+//     const cookieStore = cookies()
+//     const supabase = createRouteHandlerClient({ cookies: () => cookieStore })
+//     const { error } = await supabase.auth.exchangeCodeForSession(code)
+
+//     if (!error) {
+//       // eslint-disable-next-line no-undef
+//       return NextResponse.redirect(`${process.env.NEXT_PUBLIC_URL}/auth`)
+//     }
+//   }
+
+//   // eslint-disable-next-line no-undef
+//   return NextResponse.redirect(`${process.env.NEXT_PUBLIC_URL}/auth`)
+// }
 import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs"
 import { cookies } from "next/headers"
 import { NextResponse } from "next/server"
 
 export async function GET(request) {
-  const { searchParams, origin } = new URL(request.url)
-  const code = searchParams.get("code")
-  const next = searchParams.get("next") ?? "/"
+  const requestUrl = new URL(request.url)
+  const code = requestUrl.searchParams.get("code")
 
-  console.log(code, next)
   if (code) {
     const cookieStore = cookies()
     const supabase = createRouteHandlerClient({ cookies: () => cookieStore })
-    const { error } = await supabase.auth.exchangeCodeForSession(code)
-
-    if (!error) {
-      return NextResponse.redirect(`${origin}/auth`)
-    }
+    await supabase.auth.exchangeCodeForSession(code)
   }
 
-  return NextResponse.redirect(`${origin}/auth`)
+  // URL to redirect to after sign in process completes
+  return NextResponse.redirect(requestUrl.origin + "/auth")
 }
+
