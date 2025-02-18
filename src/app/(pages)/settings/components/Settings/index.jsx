049931cad94be44e1eb45f9737347836f053a88f
Ramz001 <rkenjaev1@gmail.com>
2025-02-18 16:26:06 +0500

    set user data settings tab ui complete

--- src/app/(pages)/settings/components/Settings/index.jsx
+++ src/app/(pages)/settings/components/Settings/index.jsx
@@ -1,227 +1,3 @@
-// 'use client'
-// import './datepicker.scss'
-// import UploadFile from 'components/Modals/UploadFile'
-// import Image from 'next/image'
-// import DatePicker from 'react-datepicker'
-// import { useUpdateUserData } from 'app/hooks/user/useUpdateUserData/useUpdateUserData'
-// import { useTranslation } from 'react-i18next'
-// import { useSelector } from 'react-redux'
-// import { toast } from 'react-toastify'
-// import { useState } from 'react'
-// import { selectUserTable } from 'app/lib/features/auth/auth.selector'
-// import { Image as ImageIcon } from 'lucide-react'
-// import {
-//   Select,
-//   SelectContent,
-//   SelectItem,
-//   SelectTrigger,
-//   SelectValue,
-// } from '@/components/ui/select'
-// import { Textarea } from '@/components/ui/textarea'
-// import { Input } from '@/components/ui/input'
-// import { SettingsContainer } from '../Container'
-// import { PhoneInput } from 'components/PhoneInput'
-// import { Button } from '@/components/ui/button'
-
-// const CabinetSettingsTab = ({ setHomeTab }) => {
-//   const { t } = useTranslation()
-//   const userTable = useSelector(selectUserTable)
-//   const [date, setDate] = useState(userTable?.birth_date ?? new Date())
-//   const [phone, setPhone] = useState(userTable?.phone)
-//   const [firstName, setFirstName] = useState(userTable?.name ?? '')
-//   const [lastName, setLastName] = useState(userTable?.last_name ?? '')
-//   const [middleName, setMiddleName] = useState(userTable?.middle_name ?? '')
-//   const [bio, setBio] = useState(userTable?.bio ?? '')
-//   const [gender, setGender] = useState(userTable?.gender ?? GENDERS.UNSET)
-//   const [isModalOpen, setModalOpen] = useState(false)
-//   const { updateUserData, isLoading, error } = useUpdateUserData()
-
-//   const handleSubmit = async (e) => {
-//     e.preventDefault()
-
-//     if (!firstName) {
-//       toast.warning(t('Iltimos ismni kiriting'), { theme: 'dark' })
-//     }
-
-//     if (!gender) {
-//       toast.warning(t('Iltimos jiningizni tanlang'), { theme: 'dark' })
-//     }
-//     await updateUserData(firstName, lastName, middleName, bio, gender, date)
-//     if (!isLoading && !error) {
-//       setHomeTab()
-//     }
-//   }
-
-//   return (
-//     <>
-//       <UploadFile isModalOpen={isModalOpen} setModalOpen={setModalOpen} />
-//       <SettingsContainer>
-//         <h3 className="mb-2 text-xl font-bold capitalize tracking-tight text-neutral-100">
-//           {t('Profil sozlamalari')}
-//         </h3>
-//         <form className="flex flex-col" onSubmit={handleSubmit}>
-//           <section className="flex w-full flex-col gap-2 sm:flex-row">
-//             <div
-//               onClick={() => setModalOpen(true)}
-//               className="group flex size-32 cursor-pointer flex-col items-center justify-center gap-1 rounded-lg border-2 border-dashed border-neutral-400 bg-stone-900 transition-all hover:bg-primary/10"
-//             >
-//               <ImageIcon className="filter-neutral-200 size-8 self-center" />
-//               <p className="mx-0.5 break-words text-center text-[11px] text-neutral-300 md:text-xs">
-//                 {t('Rasmni yuklash')}
-//               </p>
-//             </div>
-//             <div className="w-full space-y-1 sm:w-auto sm:min-w-80 md:space-y-2">
-//               <div className="w-full">
-//                 <label
-//                   className="block text-sm font-bold capitalize text-neutral-300"
-//                   htmlFor="gender"
-//                 >
-//                   {t('Telefon raqam')}
-//                 </label>
-//                 <PhoneInput
-//                   placeholder={userTable?.phone}
-//                   onChange={(e) => setPhone(e.target.value)}
-//                   value={phone}
-//                 />
-//               </div>
-//               <Button>{t('Tasdiqlash')}</Button>
-//             </div>
-
-//             <div className="w-full space-y-1 sm:w-auto sm:min-w-80 md:space-y-2">
-//               <div className="w-full">
-//                 <label
-//                   className="block text-sm font-bold capitalize text-neutral-300"
-//                   htmlFor="gender"
-//                 >
-//                   {t('Jins')}
-//                 </label>
-//                 <Select value={gender} onValueChange={setGender}>
-//                   <SelectTrigger className="h-10 w-full rounded border border-neutral-400 bg-stone-900 text-sm capitalize text-neutral-200 placeholder:text-neutral-300 md:text-base">
-//                     <SelectValue placeholder={t('Jins tanlang')} />
-//                   </SelectTrigger>
-//                   <SelectContent className="bg-neutral-900 capitalize">
-//                     <SelectItem value={GENDERS.UNSET}>
-//                       {t('Belgilanmagan')}
-//                     </SelectItem>
-//                     <SelectItem value={GENDERS.MALE}>{t('Erkak')}</SelectItem>
-//                     <SelectItem value={GENDERS.FEMALE}>{t('Ayol')}</SelectItem>
-//                   </SelectContent>
-//                 </Select>
-//               </div>
-//               <div className="flex w-full flex-col">
-//                 <label
-//                   className="block text-sm font-bold text-neutral-300"
-//                   htmlFor="birthdate"
-//                 >
-//                   {t("Tug'ilgan kuni")}
-//                 </label>
-//                 <DatePicker
-//                   id="birthdate"
-//                   selected={date}
-//                   className="h-10 w-full rounded border border-neutral-400 bg-stone-900 px-2 text-sm text-neutral-200 placeholder:text-neutral-300 md:text-base"
-//                   onChange={(date) => setDate(date)}
-//                 />
-//               </div>
-//             </div>
-//           </section>
-//           <section className="grid grid-cols-1 gap-x-2 gap-y-0 lg:grid-cols-2">
-//             <div className="col-span-2 w-full lg:col-span-1">
-//               <label
-//                 className="my-1 block text-sm font-bold capitalize text-neutral-300"
-//                 htmlFor="firstName"
-//               >
-//                 {t('Ism')}
-//               </label>
-//               <Input
-//                 placeholder={userTable?.name ?? t('Ism')}
-//                 id="firstName"
-//                 name="firstName"
-//                 type="text"
-//                 onChange={(e) => setFirstName(e.target.value)}
-//                 value={firstName}
-//                 className="h-10 w-full rounded border border-neutral-400 bg-stone-900 px-2 text-sm text-neutral-200 placeholder:text-neutral-300 md:h-12 md:text-base"
-//               />
-//             </div>
-//             <div className="col-span-2 w-full lg:col-span-1">
-//               <label
-//                 className="my-1 block text-sm font-bold capitalize text-neutral-300"
-//                 htmlFor="lastName"
-//               >
-//                 {t('Familiya')}
-//               </label>
-//               <Input
-//                 id="lastName"
-//                 name="lastName"
-//                 placeholder={userTable?.last_name ?? t('Familiya')}
-//                 type="text"
-//                 onChange={(e) => setLastName(e.target.value)}
-//                 className="h-10 w-full rounded border border-neutral-400 bg-stone-900 px-2 text-sm text-neutral-200 placeholder:text-neutral-300 md:h-12 md:text-base"
-//               />
-//             </div>
-//             <div className="col-span-2 w-full lg:col-span-1">
-//               <label
-//                 className="my-1 block text-sm font-bold capitalize text-neutral-300"
-//                 htmlFor="surname"
-//               >
-//                 {t('Sharif')}
-//               </label>
-//               <Input
-//                 id="surname"
-//                 name="surname"
-//                 placeholder={userTable?.midde_name ?? t('Sharif')}
-//                 type="text"
-//                 value={middleName}
-//                 onChange={(e) => setMiddleName(e.target.value)}
-//                 className="h-10 w-full rounded border border-neutral-400 bg-stone-900 px-2 text-sm text-neutral-200 placeholder:text-neutral-300 md:h-12 md:text-base"
-//               />
-//             </div>
-//             <div className="col-span-2 w-full">
-//               <label
-//                 className="my-1 block text-sm font-bold capitalize text-neutral-300"
-//                 htmlFor="bio"
-//               >
-//                 {t('Siz haqingizda')}
-//               </label>
-//               <Textarea
-//                 id="bio"
-//                 placeholder={t('Bio')}
-//                 value={bio}
-//                 className="min-h-36 w-full border border-neutral-400 bg-stone-900 p-2 text-neutral-200 placeholder:text-neutral-300 md:text-base"
-//                 onChange={(e) => setBio(e.target.value)}
-//                 rows={5}
-//               />
-//             </div>
-//           </section>
-//           <button
-//             className="mt-2 h-10 w-full rounded border border-black bg-primary/75 text-sm font-semibold text-neutral-900 transition-all hover:bg-primary hover:bg-opacity-100 xs:max-w-48"
-//             type="submit"
-//             disabled={isLoading}
-//           >
-//             {isLoading ? (
-//               <Image
-//                 src="/icons/loading.svg"
-//                 width={24}
-//                 height={24}
-//                 alt="loading"
-//                 className="filter-neutral-950 mx-auto size-6 animate-spin"
-//               />
-//             ) : (
-//               t('Saqlash')
-//             )}
-//           </button>
-//         </form>
-//       </SettingsContainer>
-//     </>
-//   )
-// }
-
-// const GENDERS = {
-//   UNSET: 'unset',
-//   MALE: 'male',
-//   FEMALE: 'female',
-// }
-
-// export default CabinetSettingsTab
 'use client'
 import { useState } from 'react'
 import { useSelector } from 'react-redux'
@@ -246,6 +22,7 @@ import {
   SelectValue,
 } from '@/components/ui/select'
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import { getUrl } from 'app/utils/static.util'
 
 import './datepicker.scss'
 
@@ -280,11 +57,18 @@ const CabinetSettingsTab = ({ setHomeTab }) => {
       toast.warning(t('Iltimos jiningizni tanlang'), { theme: 'dark' })
       return
     }
+
+    if (phone !== userTable?.phone) {
+      return toast.warning(t("Can't change phone yet"))
+    }
+
     await updateUserData(firstName, lastName, middleName, bio, gender, date)
     if (!isLoading && !error) {
       setHomeTab()
     }
   }
+
+  console.log(userTable?.phone)
 
   return (
     <>
@@ -297,7 +81,11 @@ const CabinetSettingsTab = ({ setHomeTab }) => {
           <div className="flex flex-col items-start gap-4 sm:flex-row">
             <div className="relative">
               <Avatar className="size-32 border-2 border-neutral-700 xl:size-36">
-                <AvatarImage src={userTable?.photo} alt={userTable?.name} />
+                <AvatarImage
+                  className="object-cover object-top"
+                  src={getUrl(userTable?.photo)}
+                  alt={userTable?.name}
+                />
                 <AvatarFallback className="bg-neutral-800 text-neutral-200">
                   {userTable?.name?.[0]}
                 </AvatarFallback>
@@ -306,14 +94,14 @@ const CabinetSettingsTab = ({ setHomeTab }) => {
                 type="button"
                 variant="secondary"
                 size="icon"
-                className="absolute bottom-0 right-0 bg-neutral-800 hover:bg-neutral-700"
+                className="absolute bottom-0 right-0 border border-neutral-500 bg-neutral-800 hover:bg-neutral-700"
                 onClick={() => setModalOpen(true)}
               >
                 <Camera className="h-4 w-4 text-neutral-200" />
               </Button>
             </div>
             <div className="w-full flex-1 space-y-4">
-              <section className="grid grid-cols-1 gap-2 sm:grid-cols-2 lg:grid-cols-3">
+              <section className="grid grid-cols-1 gap-2 sm:grid-cols-2 xl:grid-cols-3">
                 <article className="space-y-1">
                   <label
                     className="text-sm font-medium text-neutral-300"
@@ -362,6 +150,34 @@ const CabinetSettingsTab = ({ setHomeTab }) => {
                 <article className="space-y-1">
                   <label
                     className="text-sm font-medium text-neutral-300"
+                    htmlFor="phone"
+                  >
+                    {t('Telefon raqam')}
+                  </label>
+                  <PhoneInput
+                    placeholder={userTable?.phone}
+                    onChange={(value) => setPhone(value)}
+                    value={phone}
+                    className="border-neutral-700 bg-neutral-800"
+                  />
+                </article>
+                <article className="flex flex-col flex-wrap justify-between">
+                  <label
+                    className="block text-sm font-medium text-neutral-300"
+                    htmlFor="birthdate"
+                  >
+                    {t("Tug'ilgan kuni")}
+                  </label>
+                  <DatePicker
+                    id="birthdate"
+                    selected={date}
+                    onChange={(date) => setDate(date)}
+                    className="h-10 min-w-full flex-1 rounded-md border border-neutral-700 bg-neutral-800 p-2 text-sm text-neutral-200 placeholder:text-neutral-500 focus:outline-none focus:ring-1 focus:ring-neutral-300 focus:ring-offset-2 focus:ring-offset-neutral-900"
+                  />
+                </article>
+                <article className="space-y-1">
+                  <label
+                    className="text-sm font-medium text-neutral-300"
                     htmlFor="gender"
                   >
                     {t('Jins')}
@@ -373,7 +189,7 @@ const CabinetSettingsTab = ({ setHomeTab }) => {
                     >
                       <SelectValue placeholder={t('Jins tanlang')} />
                     </SelectTrigger>
-                    <SelectContent className="border-neutral-700 bg-neutral-800">
+                    <SelectContent className="border-neutral-700 bg-neutral-900">
                       <SelectItem
                         value={GENDERS.UNSET}
                         className="text-neutral-200"
@@ -395,41 +211,9 @@ const CabinetSettingsTab = ({ setHomeTab }) => {
                     </SelectContent>
                   </Select>
                 </article>
-                <article className="flex flex-col justify-between">
-                  <label
-                    className="block text-sm font-medium text-neutral-300"
-                    htmlFor="birthdate"
-                  >
-                    {t("Tug'ilgan kuni")}
-                  </label>
-                  <DatePicker
-                    id="birthdate"
-                    selected={date}
-                    onChange={(date) => setDate(date)}
-                    className="h-10 min-w-full flex-1 rounded-md border border-neutral-700 bg-neutral-800 p-2 text-sm text-neutral-200 placeholder:text-neutral-500 focus:outline-none focus:ring-1 focus:ring-neutral-300 focus:ring-offset-2 focus:ring-offset-neutral-900"
-                  />
-                </article>
               </section>
             </div>
           </div>
-
-          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
-            <article className="space-y-1">
-              <label
-                className="text-sm font-medium text-neutral-300"
-                htmlFor="phone"
-              >
-                {t('Telefon raqam')}
-              </label>
-              <PhoneInput
-                placeholder={userTable?.phone}
-                onChange={(value) => setPhone(value)}
-                value={phone}
-                className="bg-neutral-800"
-              />
-            </article>
-          </div>
-
           <div className="space-y-1">
             <label
               className="text-sm font-medium text-neutral-300"
@@ -442,21 +226,20 @@ const CabinetSettingsTab = ({ setHomeTab }) => {
               placeholder={t('Bio')}
               value={bio}
               onChange={(e) => setBio(e.target.value)}
-              rows={6}
-              className="border-neutral-700 bg-neutral-800 text-neutral-200 placeholder:text-neutral-500"
+              className="h-48 border-neutral-700 bg-neutral-800 text-neutral-200 placeholder:text-neutral-500 lg:h-56 xl:h-64"
             />
           </div>
-          <button
-            className="mt-2 h-10 w-full rounded border border-black bg-primary/75 text-sm font-semibold text-neutral-900 transition-all hover:bg-primary hover:bg-opacity-100 xs:max-w-48"
+          <Button
+            className="h-10 w-full rounded border border-black bg-primary/75 text-sm font-semibold text-neutral-900 transition-all hover:bg-primary hover:bg-opacity-100 xs:max-w-48"
             type="submit"
             disabled={isLoading}
           >
-            {!isLoading ? (
+            {isLoading ? (
               <Loader className="mx-auto animate-spin" />
             ) : (
               t('Saqlash')
             )}
-          </button>
+          </Button>
         </form>
       </SettingsContainer>
     </>
---
+++ src/components/DatePicker/index.jsx
@@ -0,0 +1,82 @@
+'use client'
+
+import * as React from 'react'
+import { addDays, format, setYear } from 'date-fns'
+import { Calendar as CalendarIcon } from 'lucide-react'
+
+import { cn } from '@/lib/utils'
+import { Button } from '@/components/ui/button'
+import { Calendar } from '@/components/ui/calendar'
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/ui/popover'
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select'
+import { formatDate } from 'app/utils/formatDate.util'
+
+export function DatePickerWithPresets() {
+  const [date, setDate] = React.useState(Date.now())
+
+  const currentYear = new Date().getFullYear()
+  const fromYear = currentYear - 100
+
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button
+          variant={'outline'}
+          className={cn(
+            'w-72 justify-start text-left font-normal',
+            !date && 'text-muted-foreground'
+          )}
+        >
+          <CalendarIcon className="mr-1" />
+          {date ? formatDate(date, 'news') : <span>Pick a date</span>}
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="flex w-auto flex-col space-y-2 p-2">
+        <div className="flex gap-2">
+          <Select onValueChange={() => {}}>
+            <SelectTrigger className="h-8 w-1/2 ring-1">
+              <SelectValue placeholder="Select" />
+            </SelectTrigger>
+            <SelectContent position="Month">
+              {Array.from({ length: 12 }, (_, i) => (
+                <SelectItem key={i} value={i + 1}>
+                  {format(addDays(new Date(fromYear, i), 1), 'LLL')}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
+          <Select onValueChange={(event) => setYear(date, event)}>
+            <SelectTrigger className="h-8 w-1/2">
+              <SelectValue placeholder="Year" />
+            </SelectTrigger>
+            <SelectContent position="popper">
+              {Array.from({ length: 100 }, (_, i) => (
+                <SelectItem key={i} value={i + fromYear}>
+                  {i + fromYear}
+                </SelectItem>
+              )).reverse()}
+            </SelectContent>
+          </Select>
+        </div>
+        <div className="rounded-md border">
+          <Calendar
+            mode="single"
+            selected={date}
+            className={''}
+            onSelect={setDate}
+          />
+        </div>
+      </PopoverContent>
+    </Popover>
+  )
+}
