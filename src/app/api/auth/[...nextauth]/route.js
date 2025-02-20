eec3c810bb060ee47a4754673127e5d383872970
Ramz001 <rkenjaev1@gmail.com>
2025-02-20 23:56:50 +0500

    next auth set up

---
+++ src/app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,34 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+import NextAuth from "next-auth/next";
+import GoogleProvider from "next-auth/providers/google";
+import YandexProvider from "next-auth/providers/yandex";
+import FacebookProvider from "next-auth/providers/facebook";
+
+const handler = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID ?? "",
+      clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET ?? "",
+    }),
+    // YandexProvider({
+    //   clientId: process.env.NEXT_PUBLIC_YANDEX_ID ?? "",
+    //   clientSecret: process.env.NEXT_PUBLIC_YANDEX_SECRET ?? "",
+    //   authorization: {
+    //     params: {
+    //       force_confirm: "yes",
+    //     },
+    //   },
+    // }),
+    // FacebookProvider({
+    //   clientId: process.env.NEXT_PUBLIC_FACEBOOK_ID ?? "",
+    //   clientSecret: process.env.NEXT_PUBLIC_FACEBOOK_SECRET ?? "",
+    // }),
+  ],
+  adapter: SupabaseAdapter({
+    url: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    secret: process.env.NEXT_PUBLIC_SUPABASE_SERVICE_ROLE,
+  }),
+});
+
+export { handler as GET, handler as POST };
