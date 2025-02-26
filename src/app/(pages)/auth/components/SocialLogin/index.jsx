2292c18d716aa8347a80ee8cb71036dc388d3915
Ramz001 <rkenjaev1@gmail.com>
2025-02-26 14:24:49 +0500

    oath2 login with credentials

--- src/app/(pages)/auth/components/SocialLogin/index.jsx
+++ src/app/(pages)/auth/components/SocialLogin/index.jsx
@@ -1,5 +1,5 @@
 import GoogleSignIn from './Google'
-import FacebookSignIn from './Facebook'
+// import FacebookSignIn from './Facebook'
 import { useTranslation } from 'react-i18next'
 import { memo } from 'react'
 import SetPhoneNumber from 'components/Modals/SetPhoneNumber'
