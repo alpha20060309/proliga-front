ad868d012b867adc438c9d2ae13d94bc740a6cb1
Ramz001 <rkenjaev1@gmail.com>
2025-02-26 10:44:28 +0500

    prisma set up

--- src/lib/db/index.js
+++
@@ -1,9 +0,0 @@
-import { drizzle } from "drizzle-orm/postgres-js"
-import postgres from "postgres"
-import * as schema from "./schema"
-
-// eslint-disable-next-line no-undef
-const connectionString = process.env.NEXT_PUBLIC_DATABASE_URL
-const client = postgres(connectionString)
-export const db = drizzle(client, { schema })
-
