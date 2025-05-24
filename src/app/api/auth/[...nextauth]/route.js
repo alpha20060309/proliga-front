bbf97d151c2d5787e14cd9ab6db1d986819e3b3b
Ramz001 <rkenjaev1@gmail.com>
2025-05-24 21:50:13 +0500

    fixing auth js fetch data from fetch on session change to fetch on jwt chagne

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -25,24 +25,23 @@ export const {
         if (dbUser) {
           token.id = dbUser.id;
           token.email = dbUser.email;
-          token.phone = dbUser.phone;
-          token.isOAuth = dbUser.isOAuth;
-          token.birth_date = dbUser.birth_date;
-          token.name = dbUser.name;
-          token.last_name = dbUser.last_name;
-          token.middle_name = dbUser.middle_name;
-          token.gender = dbUser.gender;
-          token.bio = dbUser.bio;
-          token.balance = dbUser.balance;
-          token.image = dbUser.image;
-          token.deleted_at = dbUser.deleted_at;
-          token.language = dbUser.language || LANGUAGE.uz;
-          token.phone_verified = dbUser.phone_verified;
-          token.location = dbUser.location;
-          token.ntf_token = dbUser.ntf_token;
-          token.ntf_topics = dbUser.ntf_topics;
-          token.ntf_enabled = dbUser.ntf_enabled;
-          token.ntf_token_created_at = dbUser.ntf_token_created_at;
+          token.phone = dbUser?.phone;
+          token.isOAuth = dbUser?.isOAuth;
+          token.birth_date = dbUser?.birth_date;
+          token.name = dbUser?.name;
+          token.last_name = dbUser?.last_name;
+          token.middle_name = dbUser?.middle_name;
+          token.gender = dbUser?.gender;
+          token.bio = dbUser?.bio;
+          token.balance = dbUser?.balance;
+          token.image = dbUser?.image;
+          token.language = dbUser?.language || LANGUAGE.uz;
+          token.phone_verified = dbUser?.phone_verified;
+          token.location = dbUser?.location;
+          token.ntf_token = dbUser?.ntf_token;
+          token.ntf_topics = dbUser?.ntf_topics;
+          token.ntf_enabled = dbUser?.ntf_enabled;
+          token.ntf_token_created_at = dbUser?.ntf_token_created_at;
         }
       }
 
@@ -60,7 +59,6 @@ export const {
           token.gender = dbUser.gender;
           token.bio = dbUser.bio;
           token.balance = dbUser.balance;
-          token.deleted_at = dbUser.deleted_at;
           token.language = dbUser.language || LANGUAGE.uz;
           token.phone_verified = dbUser.phone_verified;
           token.location = dbUser.location;
@@ -77,25 +75,24 @@ export const {
         if (token.sub) {
           session.user.id = +token.sub;
         }
-        session.user.email = token.email;
-        session.user.phone = token.phone;
-        session.user.isOAuth = token.isOAuth;
-        session.user.birth_date = token.birth_date;
-        session.user.name = token.name;
-        session.user.last_name = token.last_name;
-        session.user.middle_name = token.middle_name;
-        session.user.gender = token.gender;
-        session.user.bio = token.bio;
-        session.user.balance = token.balance;
-        session.user.image = token.image;
-        session.user.deleted_at = token.deleted_at;
-        session.user.language = token.language;
-        session.user.phone_verified = token.phone_verified;
-        session.user.location = token.location;
-        session.user.ntf_token = token.ntf_token;
-        session.user.ntf_topics = token.ntf_topics;
-        session.user.ntf_enabled = token.ntf_enabled;
-        session.user.ntf_token_created_at = token.ntf_token_created_at;
+        session.user.email = token?.email;
+        session.user.phone = token?.phone;
+        session.user.isOAuth = token?.isOAuth;
+        session.user.birth_date = token?.birth_date;
+        session.user.name = token?.name;
+        session.user.last_name = token?.last_name;
+        session.user.middle_name = token?.middle_name;
+        session.user.gender = token?.gender;
+        session.user.bio = token?.bio;
+        session.user.balance = token?.balance;
+        session.user.image = token?.image;
+        session.user.language = token?.language;
+        session.user.phone_verified = token?.phone_verified;
+        session.user.location = token?.location;
+        session.user.ntf_token = token?.ntf_token;
+        session.user.ntf_topics = token?.ntf_topics;
+        session.user.ntf_enabled = token?.ntf_enabled;
+        session.user.ntf_token_created_at = token?.ntf_token_created_at;
       }
       return session;
     },
