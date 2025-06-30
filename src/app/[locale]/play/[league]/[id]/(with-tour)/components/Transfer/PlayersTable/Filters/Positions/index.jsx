24069b92f0680e7d0db2e5e256d094be58a5bea3
Ramz001 <rkenjaev1@gmail.com>
2025-06-30 09:52:09 +0500

    Update localization strings in PlayersTable and enhance button styling in Clubs and Price filters for improved UI consistency.

--- src/app/[locale]/play/[league]/[id]/(with-tour)/components/Transfer/PlayersTable/Filters/Positions/index.jsx
+++ src/app/[locale]/play/[league]/[id]/(with-tour)/components/Transfer/PlayersTable/Filters/Positions/index.jsx
@@ -12,7 +12,7 @@ const PositionsFilter = ({ column, columnFilterValue }) => {
 
   const DATA = [
     {
-      title: 'Barchasi',
+      title: 'Pozitsiya',
       key: 'ALL', // Changed from empty string to 'ALL'
     },
     {
@@ -46,7 +46,7 @@ const PositionsFilter = ({ column, columnFilterValue }) => {
       className="w-full py-0"
     >
       <SelectTrigger className="focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive bg-background hover:bg-accent dark:bg-input/30 dark:border-input dark:hover:bg-input/50 hover:text-foreground w-full rounded-md border border-dashed px-3 text-sm font-medium whitespace-nowrap shadow-xs transition-all outline-none focus-visible:ring-[3px] disabled:pointer-events-none disabled:opacity-50 data-[size=default]:h-8 border-border">
-        <SelectValue placeholder={t('Barchasi')} />
+        <SelectValue placeholder={t('Pozitsiya')} />
       </SelectTrigger>
       <SelectContent>
         {DATA.map((position) => (
