8b91a10cc600664a52902318ea1e6b58cd6ae77e
Ramz001 <rkenjaev1@gmail.com>
2025-06-30 07:47:45 +0500

    Refactor teamPlayerExtraReducer to streamline player data processing. Consolidate player categorization and statistics calculation into a single reduce function, improving code readability and maintainability. Fix error handling for rejected fetchTeamPlayers action.

--- src/app/lib/features/teamPlayer/teamPlayer.extraReducer.js
+++ src/app/lib/features/teamPlayer/teamPlayer.extraReducer.js
@@ -1,5 +1,4 @@
 import { fetchTeamPlayers } from './teamPlayer.thunk'
-import { PLAYER_POSITION } from 'app/utils/player.util'
 
 export const teamPlayerExtraReducer = (builder) => {
   builder
@@ -7,72 +6,51 @@ export const teamPlayerExtraReducer = (builder) => {
       state.isLoading = true
     })
     .addCase(fetchTeamPlayers.fulfilled, (state, action) => {
-      state.GOA = []
-      state.DEF = []
-      state.MID = []
-      state.STR = []
-      state.playersCount = {
-        GOA: 0,
-        DEF: 0,
-        MID: 0,
-        STR: 0,
-      }
-      state.teamPrice = 0
-      state.duplicatesMap = {}
-      state.prevTeam = []
+      const team = action.payload.data || []
+
+      const teamData = team.reduce(
+        (acc, player) => {
+          const { position, club_id, name, price } = player
+
+          if (position && acc[position]) {
+            acc[position].push(player)
+            acc.teamPrice += price || 0
+
+            if (name) {
+              const clubSlug = club_id?.id ?? ''
+              acc.playersCount[position]++
+              acc.duplicatesMap[clubSlug] =
+                (acc.duplicatesMap[clubSlug] || 0) + 1
+            }
+          }
+          return acc
+        },
+        {
+          GOA: [],
+          DEF: [],
+          MID: [],
+          STR: [],
+          playersCount: { GOA: 0, DEF: 0, MID: 0, STR: 0 },
+          teamPrice: 0,
+          duplicatesMap: {},
+        },
+      )
+
+      state.GOA = teamData.GOA
+      state.DEF = teamData.DEF
+      state.MID = teamData.MID
+      state.STR = teamData.STR
+      state.playersCount = teamData.playersCount
+      state.teamPrice = teamData.teamPrice
+      state.duplicatesMap = teamData.duplicatesMap
+
+      state.prevTeam = team
       state.transferModal = false
       state.infoModal = false
-
-      const team = action.payload.data
-      team?.length > 0 && (state.prevTeam = team)
-      team?.length > 0 &&
-        team.forEach((player) => {
-          const clubSlug = player?.club_id?.id ?? ''
-
-          if (player.position === PLAYER_POSITION.GOA) {
-            state.GOA.push(player)
-            if (player.name) {
-              state.playersCount.GOA++
-              state.duplicatesMap[clubSlug] =
-                (state.duplicatesMap[clubSlug] || 0) + 1
-            }
-          }
-          if (player.position === PLAYER_POSITION.DEF) {
-            state.DEF.push(player)
-            if (player.name) {
-              state.playersCount.DEF++
-              state.duplicatesMap[clubSlug] =
-                (state.duplicatesMap[clubSlug] || 0) + 1
-            }
-          }
-          if (player.position === PLAYER_POSITION.MID) {
-            state.MID.push(player)
-            if (player.name) {
-              state.playersCount.MID++
-              state.duplicatesMap[clubSlug] =
-                (state.duplicatesMap[clubSlug] || 0) + 1
-            }
-          }
-          if (player.position === PLAYER_POSITION.STR) {
-            state.STR.push(player)
-            if (player.name) {
-              state.playersCount.STR++
-              state.duplicatesMap[clubSlug] =
-                (state.duplicatesMap[clubSlug] || 0) + 1
-            }
-          }
-        })
-
-      state.teamPrice =
-        state.GOA.reduce((acc, player) => acc + player.price, 0) +
-        state.DEF.reduce((acc, player) => acc + player.price, 0) +
-        state.MID.reduce((acc, player) => acc + player.price, 0) +
-        state.STR.reduce((acc, player) => acc + player.price, 0)
-
       state.isLoading = false
     })
     .addCase(fetchTeamPlayers.rejected, (state, action) => {
-      state.error = action?.error?.messagell
+      state.error = action?.error?.message
       state.isLoading = false
     })
 }
