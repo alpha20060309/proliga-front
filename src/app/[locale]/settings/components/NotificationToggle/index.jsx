632a812d8549784c737090ce3dc1d266566505c2
Ramz001 <rkenjaev1@gmail.com>
2025-07-04 09:47:47 +0500

    Notification Settings indicator

--- src/app/[locale]/settings/components/NotificationToggle/index.jsx
+++ src/app/[locale]/settings/components/NotificationToggle/index.jsx
@@ -1,3 +1,4 @@
+'use client'
 import { useTranslation } from 'react-i18next'
 import {
   Card,
@@ -6,16 +7,28 @@ import {
   CardTitle,
 } from 'components/ui/card'
 import { Switch } from 'components/ui/switch'
+import { useState, useEffect } from 'react'
 
 const NotificationToggle = () => {
   const { t } = useTranslation()
+  const [permission, setPermission] = useState('default')
+  const [isEnabled, setIsEnabled] = useState(false)
+
+  useEffect(() => {
+    if ('Notification' in window) {
+      setPermission(Notification.permission)
+      setIsEnabled(Notification.permission === 'granted')
+    }
+  }, [])
+  console.log(permission)
 
   return (
     <Card className="flex w-full flex-col gap-4 px-4 sm:max-w-64">
       <CardHeader className={'flex items-center justify-between p-0'}>
         <CardTitle>{t('Xabarnomalar')}</CardTitle>
         <Switch
-          disabled
+          checked={isEnabled}
+          disabled={!('Notification' in window) || permission === 'denied'}
           className="data-[state=checked]:bg-success data-[state=unchecked]:bg-muted h-5 w-10"
           thumbClassName="size-5 data-[state=checked]:translate-x-5"
         />
