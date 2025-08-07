e3dcca51470f3f114f83cc31b99b21f0dd94927d
Ramzbek Kenjayev <rkenjaev1@gmail.com>
2025-08-07 12:23:15 +0500

    adding working workflow

---
+++ scripts/start-server.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-undef */
+// wrapper.js
+const { exec } = require('child_process');
+exec('npm start', { cwd: process.cwd(), shell: true, windowsHide: true }, (e, stdout, stderr) => {
+  if (e) console.error(e);
+  console.log(stdout);
+  console.error(stderr);
+});
